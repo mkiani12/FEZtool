a1346e3f35ebfffa00c6b2a003a500c118ba3632
Mohammad Kiani <60611017+mkiani12@users.noreply.github.com>
2023-09-21 23:01:34 +0330

    19

--- components/Subscribe.vue
+++ components/Subscribe.vue
@@ -5,27 +5,46 @@ const { $axios } = useNuxtApp();
 const pattern =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+const form = ref(null);
 const formValid = ref(false);
 
 const name = ref(null);
 const email = ref(null);
 
 const snackbar = ref(false);
+const snackbarTimeout = ref(3000);
 let resText = "";
+
+const loading = ref(false);
 
 const formSubmit = async () => {
   const formData = { username: name.value, email: email.value };
+  loading.value = true;
 
   try {
     const { data } = await $axios.post("/auth/register", formData);
     resText = data;
     snackbar.value = true;
+    form.value.reset();
   } catch (error) {
     if (error.response) {
       resText = error.response.data;
       snackbar.value = true;
+    } else {
+      snackbarTimeout.value = 15000;
+      resText = `If you see this message, check your network. 
+        <br>
+        if you're ok then please text me in
+        <a class="text-blue-300 dark:text-blue-800" href='mailto: mohmmd@skiff.com'> mohmmd@skiff.com :)) </a>
+        <br>
+        it has to be checked`;
+      snackbar.value = true;
+      setTimeout(() => {
+        snackbarTimeout.value = 3000;
+      }, 15000);
     }
   }
+  loading.value = false;
 };
 </script>
 <template>
@@ -35,7 +54,7 @@ const formSubmit = async () => {
       follow us now! ;)
     </p>
 
-    <v-form v-model="formValid">
+    <v-form ref="form" v-model="formValid">
       <v-text-field
         v-model="name"
         class="mb-2"
@@ -50,9 +69,7 @@ const formSubmit = async () => {
         class="mb-2"
         :rules="[
           (v) => !!v || 'Email is required',
-          (v) => {
-            return pattern.test(v) || 'Email is incurrect';
-          },
+          (v) => pattern.test(v) || 'Email is incurrect',
         ]"
         clearable
         label="Email Address"
@@ -66,6 +83,7 @@ const formSubmit = async () => {
         size="large"
         block
         @click="formSubmit"
+        :loading="loading"
       >
         Subscribe
       </v-btn>
@@ -75,9 +93,13 @@ const formSubmit = async () => {
       And don’t worry, we hate spam too! You can unsubcribe at anytime.
     </p>
 
-    <v-snackbar v-model="snackbar" location="bottom left" class="mb-5 ml-5">
-      {{ resText }}
-
+    <v-snackbar
+      v-model="snackbar"
+      location="bottom left"
+      class="mb-5 ml-5"
+      :timeout="snackbarTimeout"
+    >
+      <span v-html="resText"></span>
       <template v-slot:actions>
         <v-btn color="pink" variant="text" @click="snackbar = false">
           Close
